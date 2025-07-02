d5de125923a2927b812a36e87cf5cd53d2fc9100
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-07-02 15:16:09 +0900

    feat: MUI customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { light } from "@/scss/MaterialTheme";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log("_APP main file");
+  //@ts-ignore
+  const [theme, setTheme] = useState(createTheme(light));
 
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
-export default function MainPage() {
-  console.log("route main file");
+import { Box, Container, Stack } from "@mui/material";
+import { brown, green } from "@mui/material/colors";
 
-  return <div>MAIN PAGE</div>;
+export default function Home() {
+  return (
+    <>
+      <Stack sx={{ background: "#81c784" }}>Header</Stack>
+      <Container>
+        <Stack flexDirection={"column"}>
+          <Box>Popular Properties</Box>
+          <Box>Top Agents</Box>
+          <Box>Top Properties</Box>
+          <Box>Events</Box>
+        </Stack>
+      </Container>
+      <Stack sx={{ background: "#a1887f" }}>Footer</Stack>
+    </>
+  );
 }
