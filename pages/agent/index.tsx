f20dbf096a0e88d5ccc50f69fc54bca0f63885b5
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-07-04 15:56:14 +0900

    fix: refactor pages components

--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -3,12 +3,12 @@ import { Container, Stack } from "@mui/material";
 import { NextPage } from "next";
 import { useState } from "react";
 
-const Community: NextPage = () => {
+const AgentList: NextPage = () => {
   return (
     <div style={{ margin: "20px 0" }}>
-      <Stack className="container">COMMUNITY</Stack>
+      <Stack className="container">AGENT LIST</Stack>
     </div>
   );
 };
 
-export default withLayotBasic(Community);
+export default withLayotBasic(AgentList);
--- pages/cs/index.tsx
+++ pages/cs/index.tsx
@@ -3,12 +3,12 @@ import { Container, Stack } from "@mui/material";
 import { NextPage } from "next";
 import { useState } from "react";
 
-const Community: NextPage = () => {
+const CS: NextPage = () => {
   return (
     <div style={{ margin: "20px 0" }}>
-      <Stack className="container">COMMUNITY</Stack>
+      <Stack className="container">CS</Stack>
     </div>
   );
 };
 
-export default withLayotBasic(Community);
+export default withLayotBasic(CS);
